f083f482972bacb8745c1c1ad4483de50fa2e6b9
cagleeh <cagleeh@mail.uc.edu>
2024-11-19 13:10:05 -0600

    Update TeamMemberEC.cs

--- FinalProjectGroup2/Models/TeamMemberEC.cs
+++ FinalProjectGroup2/Models/TeamMemberEC.cs
@@ -2,5 +2,9 @@
 {
     public class TeamMemberEC
     {
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string CollegeProgram { get; set; }
+        public string YearInProgram { get; set; }
     }
 }
