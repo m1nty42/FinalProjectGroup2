89a0e80fe669fee57ce991e59cf25d3b89b6b9fd
Eli Cagle <cagleeh@mail.uc.edu>
2024-11-19 13:40:17 -0600

    test

---
+++ FinalProjectGroup2/Controllers/TeamMembersControllerEC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinalProjectGroup2.Data;
+using FinalProjectGroup2.Models;
+
+namespace FinalProjectGroup2.Controllers { 
+
+    [Route("api/[controller]")]
+    [ApiController]
+{
+    public class TeamMembersControllerEC : Controller
+    {
+    }
+}
